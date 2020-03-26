d7cc719359114fee6ec8c6b0ee8bbe430cdb7b71
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-03-25 18:47:49 -0600

    Todos can now be manually added by user

--- src/components/screens/list_view_screen.js
+++ src/components/screens/list_view_screen.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
-import { Container, Text, H1 } from 'native-base';
-import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { Container, Input, Item, Text, H1 } from 'native-base';
+import { Alert, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import _ from 'lodash';
 import TodoListItem from '../todos/todo_list_item';
 import { createTodo, markTodoDone } from '../../actions/lists';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class ListScreen extends React.Component {
   styles = StyleSheet.create({
     message: {
       alignItems: 'center',
       padding: 16,
-    }
+    },
+    inputBox: {
+      marginLeft: 16,
+      marginRight: 16,
+      marginVertical: 16,
+    },
+    addButton: {
+      marginRight: 20,
+    },
   })
 
   state = {
     loadedName: 'Stu',
+    newTodo: '',
   }
 
-  componentDidMount() {
-    this.props.createTodo('Hello World', this.props.list.id);
+  update = (key, value) => this.setState({ [key]: value })
+
+  addTodo = () => {
+    if (this.state.newTodo === '') {
+      Alert.alert(
+        'Missing Info',
+        'You have to provide a todo dummy!!',
+      );
+      return;
+    }
+    this.props.createTodo(this.state.newTodo, this.props.list.id);
+    this.setState({newTodo: ''});
   }
 
   markDone = (item) => {
@@ -28,17 +48,22 @@ export class ListScreen extends React.Component {
   }
 
   render() {
-    if (this.props.list.todos.length === 0) {
-      return (
-        <Container style={this.styles.message}>
-          <H1>Welcome {this.state.loadedName}!</H1>
-          <Text>You do not have any todos yet, click the "New" button at the top to add a new todo.</Text>
-        </Container>
-      )
-    }
-
     return (
       <Container>
+        <Item rounded style={this.styles.inputBox}>
+          <Input 
+            placeholder='New Todo' 
+            value={this.state.newTodo}
+            onChangeText={text => this.update('newTodo', text)}
+          />
+          <Icon 
+            name="plus" 
+            size={24} 
+            color="blue" 
+            style={this.styles.addButton}
+            onPress={() => this.addTodo()}
+          />
+        </Item>
         <FlatList
           data={this.props.list.todos}
           renderItem={({item}) => (
